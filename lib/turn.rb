23f241b1c13e002b3f60223f846807021b18b529
Daniel Kim <daniel.kim52@gmail.com>
2019-10-24 19:54:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,11 @@ end
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
